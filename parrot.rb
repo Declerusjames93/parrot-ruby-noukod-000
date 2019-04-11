49726bdc9c6f2109e601fd97498bed0e7966159a
James Declerus <james.declerus@esih.edu>
2019-04-11 16:22:36 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot("Squawk!")
-  puts "Pretty bird!"
+def parrot(value = "Squawk!")
+  n_value= value("Pretty bird!")
+  puts #
   return "Pretty bird!"
 end
